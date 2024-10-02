f5563710a5a86b2111979e94a1c08868c3116b92
mateusrosa <pedrompmrosa@gmail.com>
2024-10-02 18:28:04 +0100

    demoworking

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import numpy as np
 from teams_assigment import TeamAssigment
 from player_ball_assigment import PlayerBallAssigment
 from camera_movement import CameraMovementEstimator
+from view_transformer import ViewTransformer
+from speed_and_distance_estimator import SpeedAndDistanceEstimator
 
 def main():
     video_name = "00_trimmed"
@@ -23,8 +25,18 @@ def main():
     camera_movement_per_frame = camera_movement_estimator.get_camera_movement(video_frames, read_from_stub = True, stub_path="stubs/camera_movement_stub.pkl")
     camera_movement_estimator.add_adjust_position_to_tracks(tracks,camera_movement_per_frame)
 
+
+    #View transformer 
+    view_transformer = ViewTransformer()
+    view_transformer.add_transformed_position_to_tracks(tracks)
+
     #interpolate ball positions
     tracks["ball"] = tracker.interpolate_ball_position(tracks["ball"])
+
+    #speed and distance 
+    speed_and_distance_estimator = SpeedAndDistanceEstimator()
+    speed_and_distance_estimator.add_speed_and_distance_to_tracks(tracks)
+
 
     #assign teams
     team_assigment = TeamAssigment()
@@ -72,6 +84,8 @@ def main():
     #Draw camera movement
     output_video_frames = camera_movement_estimator.draw_camera_movement(output_video_frames, camera_movement_per_frame)
     
+    #draw speed and distance
+    speed_and_distance_estimator.draw_speed_and_distance(output_video_frames, tracks)
     save_video(output_video_frames, f"output_videos/{video_name}.avi")
 
 if __name__ == "__main__":
